fdc4853d9f723659a266ac8edfa37faa8eeb8ef9
sherry <sherry.kisilu@studebnt.moringaschool.com>
2023-05-21 16:10:10 +0300

    done

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,33 @@
 class ItemsController < ApplicationController
-
+rescue_from ActiveRecord::RecordNotFound, with: :user_not_found
   def index
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      items = user.items
+      render json: items
+    else
     items = Item.all
     render json: items, include: :user
+    end
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, status: :created
+  end
+
+  private
+  def item_params
+    params.permit(:name, :description, :price)
+  end
+  def user_not_found
+    render json: {errors: "Resource Not found"}, status: :not_found
   end
 
 end
